1c80f1435a3fa7c6d95bc11209379209fd3a7629
md-shakib-hassan <arefinshakibkhan@gmail.com>
2026-02-23 23:19:18 +0600

    variable complete semi

--- variable/app.js
+++ variable/app.js
@@ -3,4 +3,26 @@ let lastName="Hassan";
 //blocked type variable use modern js
 console.log(firstName +" "+lastName);
 
+//here the constant variable which is muteable
+const pi = 3.1415;
+console.log(pi);
+
+// in let varibale container reassign is possible  and it is scope type variable
+
+firstName="hasib";
+console.log(firstName);
+
+
+// var container variable also reassign container
+
+var name="shaib";
+var name="khan";
+console.log(name);
+
+
+
+
+
+
+
 
