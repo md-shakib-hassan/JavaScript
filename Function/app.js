6a7c935fcbf2e4b0dc32eadcfcb28257a2004a96
md-shakib-hassan <arefinshakibkhan@gmail.com>
2026-02-24 23:21:15 +0600

    doing function

---
+++ Function/app.js
@@ -0,0 +1,12 @@
+console.log("Hello JavaScript.");
+
+let firstName ="Shakib";
+let age =23;
+let dept = "Software Engineering";
+
+function addDetails(firstName, age, dept){
+   console.log(firstName+ "\n"+ age + "\n"+ dept);
+
+}
+addDetails(firstName, age, dept);
+
