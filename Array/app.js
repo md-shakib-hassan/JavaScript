c338f338978d845dc866900524313a0f70a6af59
md-shakib-hassan <arefinshakibkhan@gmail.com>
2026-03-04 21:19:45 +0600

    here add after unshift

--- Array/app.js
+++ Array/app.js
@@ -23,3 +23,81 @@ console.log("dept: "+dept[3]);
 const list=dept.toString();
 document.getElementsByClassName("demo").innerHTML = list;
 
+console.log(list);
+//  there are some properties of length and sorting 
+
+// dept.lenght
+// dept.sort();
+
+console.log("the length: "+dept.length);
+
+// sort using in array
+// that importance
+
+let arr = [7,9,3,56,234,754];
+
+arr.sort((a, b) => a - b);
+
+console.log("After Sorting: " + arr);
+
+// push in array
+
+arr.push(4);
+let len =arr.length;
+
+for (let i=0; i<len; i++){
+    console.log("here: "+arr[i])
+}
+
+// here modern use in javascript and react is use forEach()
+// here forEach like traver a element 
+
+
+let arr2 =[10,20,403,40,50,540];
+
+// arr2.forEach((value)=>{
+//     console.log(value);
+// });
+
+arr2.forEach((value) => console.log(value));
+
+// using array index at() function
+console.log("at index: "+arr2.at(4));
+
+// using of array operator that is ar toString() function which also convert into string
+
+
+let array = ["ict","cse","se"];
+
+console.log(array.join());
+
+
+// pop using in array 
+
+console.log("poping: "+array.pop());
+array.forEach((value)=> console.log(value));
+
+// push function
+
+array.push("social work");
+console.log("After push");
+array.forEach((value) => console.log(value));
+
+//  using of shift
+
+let array2 = [10, 20, 30];
+
+let removed = array2.shift();
+
+console.log("Removed:", removed);
+console.log("Array now:", array2);
+
+
+// using unshift()
+
+let array3 = [20, 30, 40];
+
+let newLength = array3.unshift(10);
+
+console.log("New Length:", newLength);
+console.log("Array Now:", array3);
