f1a25cbe96b1ce4f769ffdec847d1593a8f0d15b
md-shakib-hassan <arefinshakibkhan@gmail.com>
2026-03-06 23:45:28 +0600

    string function part

--- String/app.js
+++ String/app.js
@@ -32,3 +32,56 @@ console.log(length);
     console.log(total);
 
 }
+
+// charAt() used find a char in word
+
+{
+    let text = "I am shakib , and i am a student , study SE.";
+
+    let char = text.charAt(6);
+    console.log("CHAR IS : " + char);
+}
+
+
+// here charCodeAt(); used to find that char asci value in decimal that used for it.
+
+{
+    let text = "Hello world!";
+    console.log("CharCodeAT(): " + text.charCodeAt(0));
+}
+
+
+// concat that function use for add two or more string in a string
+
+
+{
+    let name = "shakib";
+    let name2 = "hassan ";
+
+    console.log("Using concat: " + name.concat(" ", name2));
+
+}
+
+// here slice() function that create slice of string and recive it.
+
+{
+    let text = "Apple, Banana, Kiwi";
+    let part = text.slice(7, 13);
+    console.log("using slice : "+part);
+}
+
+{
+    let text = "Apple, Banana, Kiwi";
+let part = text.slice(7);
+console.log("using slice : "+part);
+}
+
+// If a parameter is negative, the position is counted from the end of the string:
+
+{
+    let text = "Apple, Banana, Kiwi";
+let part = text.slice(-12);
+ console.log("using slice : "+part);
+
+}
+
