3f8f8c3277010e4d66ff8771908b24ba114dfa3d
md-shakib-hassan <arefinshakibkhan@gmail.com>
2026-03-07 18:04:26 +0600

    here adding function of array

--- Array/app.js
+++ Array/app.js
@@ -1,26 +1,26 @@
 console.log("Hello, Lok jon.");
 
 // Start Array
-const cars=["BMW", "TOYOTA","AUDI"];
-console.log("Car Name: "+cars[2]);
+const cars = ["BMW", "TOYOTA", "AUDI"];
+console.log("Car Name: " + cars[2]);
 
-const name =[];
+const name = [];
 
-name[0]="shakib";
-name[1]="hasib";
-name[2]="nasib";
+name[0] = "shakib";
+name[1] = "hasib";
+name[2] = "nasib";
 
-console.log("name: "+name[1]);
+console.log("name: " + name[1]);
 
 
 // using new keyword to create Array
 
-const dept = new Array("ict","cste","Se","phar");
+const dept = new Array("ict", "cste", "Se", "phar");
 
-console.log("dept: "+dept[3]);
+console.log("dept: " + dept[3]);
 
 // console.log("Converting array to String :"+cars.toString); that was wrong
-const list=dept.toString();
+const list = dept.toString();
 document.getElementsByClassName("demo").innerHTML = list;
 
 console.log(list);
@@ -29,12 +29,12 @@ console.log(list);
 // dept.lenght
 // dept.sort();
 
-console.log("the length: "+dept.length);
+console.log("the length: " + dept.length);
 
 // sort using in array
 // that importance
 
-let arr = [7,9,3,56,234,754];
+let arr = [7, 9, 3, 56, 234, 754];
 
 arr.sort((a, b) => a - b);
 
@@ -43,17 +43,17 @@ console.log("After Sorting: " + arr);
 // push in array
 
 arr.push(4);
-let len =arr.length;
+let len = arr.length;
 
-for (let i=0; i<len; i++){
-    console.log("here: "+arr[i])
+for (let i = 0; i < len; i++) {
+    console.log("here: " + arr[i])
 }
 
 // here modern use in javascript and react is use forEach()
 // here forEach like traver a element 
 
 
-let arr2 =[10,20,403,40,50,540];
+let arr2 = [10, 20, 403, 40, 50, 540];
 
 // arr2.forEach((value)=>{
 //     console.log(value);
@@ -62,20 +62,20 @@ let arr2 =[10,20,403,40,50,540];
 arr2.forEach((value) => console.log(value));
 
 // using array index at() function
-console.log("at index: "+arr2.at(4));
+console.log("at index: " + arr2.at(4));
 
 // using of array operator that is ar toString() function which also convert into string
 
 
-let array = ["ict","cse","se"];
+let array = ["ict", "cse", "se"];
 
 console.log(array.join());
 
 
 // pop using in array 
 
-console.log("poping: "+array.pop());
-array.forEach((value)=> console.log(value));
+console.log("poping: " + array.pop());
+array.forEach((value) => console.log(value));
 
 // push function
 
@@ -83,7 +83,7 @@ array.push("social work");
 console.log("After push");
 array.forEach((value) => console.log(value));
 
-//  using of shift
+//  using of shift =removve first element of a array
 
 let array2 = [10, 20, 30];
 
@@ -93,7 +93,7 @@ console.log("Removed:", removed);
 console.log("Array now:", array2);
 
 
-// using unshift()
+// using unshift() = add method adds at beginning 
 
 let array3 = [20, 30, 40];
 
@@ -101,3 +101,22 @@ let newLength = array3.unshift(10);
 
 console.log("New Length:", newLength);
 console.log("Array Now:", array3);
+
+
+// join( ) used for join array to string
+
+{
+    const fruits = ["Banana", "Orange", "Apple", "Mango"];
+    console.log("Result: " + fruits.join(" * "));
+}
+
+// concatiing two or more array
+
+{
+    const myGirls = ["Cecilie", "Lone"];
+    const myBoys = ["Emil", "Tobias", "Linus"];
+    const number =[4,32,45,56];
+
+    const myChildren = myGirls.concat(myBoys,number);
+    console.log("Merging array: "+myChildren);
+}
