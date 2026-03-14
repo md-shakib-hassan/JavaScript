505bca41f398c9629780ece652333c2360bc3c93
md-shakib-hassan <arefinshakibkhan@gmail.com>
2026-03-14 18:13:35 +0600

    added DOM

---
+++ DOM/script/getElementID.js
@@ -0,0 +1,27 @@
+// start from getElementByID 
+
+console.log("Hello.");
+
+const greeding = document.getElementById('greed');
+
+greeding.innerText="JavaScript!";
+greeding.innerHTML=" JavaScript!";
+
+greeding.style.color="blue";
+greeding.style.backgroundColor="yellow";
+greeding.style.padding='5px';
+greeding.style.borderRadius='8px';
+greeding.style.paddingLeft='10px';
+
+console.log("output: ",greeding.innerText);
+
+
+// getElementSByTagName()
+
+
+const design =document.getElementsByTagName('section');
+
+design.style.backgroundColor='blue';
+
+
+
