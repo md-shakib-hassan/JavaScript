d4d12987ff9c1f10f37860d7179af735bc4b721a
md-shakib-hassan <arefinshakibkhan@gmail.com>
2026-03-28 12:30:45 +0600

    fetch() api

--- API,JSON/script/fetch.js
+++ API,JSON/script/fetch.js
@@ -4,3 +4,19 @@ const button =()=>{
       .then(response => response.json())
       .then(json => console.log(json))
 }
+
+// many user details show 
+
+
+const button2 = () => {
+  fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json)
+      .then(data => {
+        console.log(data);
+        userHandle(data);
+      })
+}
+
+const userHandle = (users)=>{
+  console.log(users);
+}
