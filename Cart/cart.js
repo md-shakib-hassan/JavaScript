894203a224755a1c4b06a1c069bd10486b059680
md-shakib-hassan <arefinshakibkhan@gmail.com>
2026-04-15 19:57:13 +0600

    cart type

---
+++ Cart/cart.js
@@ -0,0 +1,45 @@
+const addtoCart =()=>{
+
+   const productName = document.getElementById('productName');
+   const productQuantity = document.getElementById("productQuantity");
+
+   const name = productName.value;
+   const quantity = productQuantity.value;
+
+    displayProduct(name,quantity);
+
+    productName.value="";
+    productQuantity.value="";
+    
+}
+
+const displayProduct = (productName,productQuantity)=>{
+
+    const container = document.getElementById('cartContainer');
+
+   const li = document.createElement('li');
+
+   li.innerText=`ProductName: ${productName} || Quantity: ${productQuantity}
+     
+   `
+   container.appendChild(li);
+
+ cart(li.innerText);
+
+}
+
+const cart =(products)=>{
+
+    const product= JSON.stringify(products);
+
+     localStorage.setItem("cart",product);
+
+     const additem = localStorage.getItem(cart);
+
+     additem
+
+     console.log()
+
+    
+
+}
